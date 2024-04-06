600a1acf44d08aa44134e5242eb83d3e83102f54
mo11om <66251276+mo11om@users.noreply.github.com>
2024-04-06 18:02:35 +0800

    example sql

---
+++ example/insert_condition.sql
@@ -0,0 +1,8 @@
+-- Replace `<ad_id>` with the actual ad ID (retrieved in step 2 or your existing ad)
+INSERT INTO AdConditions (ad_id, condition_id)
+SELECT 1, c.id
+FROM Conditions c
+WHERE  
+  OR c.type = 'gender' AND c.value IN ('M', 'F')  -- Target both genders
+  OR c.type = 'country' AND c.value IN ('TW', 'JP')  -- Target both countries (Taiwan & Japan)
+  OR c.type = 'platform' AND c.value IN ('android', 'ios', 'web');  -- Target all platforms
---
+++ example/serach.sql
@@ -0,0 +1,22 @@
+SELECT a.id, a.title, a.start_at, a.end_at
+FROM Ads a 
+INNER JOIN AdConditions ac ON a.id = ac.ad_id 
+INNER JOIN Conditions c ON ac.condition_id = c.id
+
+WHERE
+ a.start_at <= NOW() AND a.end_at >= NOW() 
+AND 
+(a.min_age <= 24 AND a.max_age >= 24)
+AND 
+
+((c.type = 'gender' AND c.value = ('F')) 
+OR (c.type = 'country' AND c.value = ('TW')) 
+OR (c.type = 'platform' AND c.value = ('ios')) )
+
+group by ad_id 
+having  COUNT(*)>3
+ORDER by end_at ASC
+
+
+
+ 
