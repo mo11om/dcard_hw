4211989727bb5476dd5101e3b7f503f3605ee4c8
mo11om <66251276+mo11om@users.noreply.github.com>
2024-04-07 11:14:05 +0800

    write comment

--- src/controllers/controller_ad.go
+++ src/controllers/controller_ad.go
@@ -10,6 +10,7 @@ import (
 
 var redisClient *Client
 
+// init redis client
 func Init_redis() error {
 	cfg := &Config{
 		Addr:     "localhost:6379",
@@ -25,13 +26,98 @@ func Init_redis() error {
 	return nil
 }
 
-// Database connection (replace with your actual connection logic)
-// var db *sql.DB
-// db, err :=
+// redis operation
+// flush all ad record in redis
+func flushAllAdsCache() error {
 
+	// Implement logic to connect to Redis and flush the cache (e.g., using FlushAll)
+	// Consider alternative cache invalidation strategies for better performance
+
+	return redisClient.FlushAll(context.Background()) // Consider setting a suitable expiration time // Replace with actual implementation
+}
+
+// set model.result with key  "Age_Gender_Country_Platform"
+func redisSetAd(key string, ads []model.Result) error {
+	data, err := json.Marshal(ads)
+
+	if err != nil {
+		return err
+	}
+	return redisClient.Set(context.Background(), key, data, 0) // Consider setting a suitable expiration time
+}
+
+// get model.result  with key  "Age_Gender_Country_Platform"
+
+func redisGetAd(key string) ([]model.Result, error) {
+	result, err := redisClient.Get(context.Background(), key)
+	// fmt.Println("get error", err)
+	if result == "" {
+		// fmt.Println("leave nil")
+		return nil, err // Key not found in cache
+
+	} else if err != nil {
+		// fmt.Println("leave != nil")
+
+		return nil, err
+	}
+	// fmt.Println(" success get res")
+	var ads []model.Result
+	if err := json.Unmarshal([]byte(result), &ads); err != nil {
+		return nil, err
+	}
+	// fmt.Println(key, "get ", ads)
+
+	return ads, nil
+}
+
+//
+// clause
+//
+
+// get_clause constructs a comma-separated string of single-quoted values from a slice of strings.
+// It's typically used to generate SQL IN clauses for conditions.
+//
+// It takes a slice of strings as input and returns a single string in the format:
+// 'value1', 'value2', ..., 'valueN'
+func get_clause(data []string) string {
+
+	Clause := ""
+	for _, Code := range data {
+		Clause += "'" + Code + "',"
+	}
+	Clause = Clause[:len(Clause)-1] // Remove trailing comma
+	return Clause
+}
+
+// getSearchString generates a search string by combining condition fields.
+// It takes a Search_Condition struct and returns a string suitable for search purposes.
+//
+// The generated string format is: "<age>_<gender>_<country>_<platform>".
+// - `<age>`: The value of the `Age` field in the `Search_Condition` struct.
+// - `<gender>`: The value of the `Gender` field in the `Search_Condition` struct.
+// - `<country>`: The value of the `Country` field in the `Search_Condition` struct.
+// - `<platform>`: The value of the `Platform` field in the `Search_Condition` struct.
+//
+// This function is likely used to create a unique identifier for search combinations
+// or for building search queries based on the provided conditions.
+func getSearchString(condition model.Search_Condition) string {
+	return fmt.Sprintf("%d_%s_%s_%s", condition.Age, condition.Gender, condition.Country, condition.Platform)
+}
+
+//
+// database dealing
+//
+
+//create ads
+
+// CreateAd creates a new ad record in the database and its associated conditions.
+// It takes an Ad struct containing the ad details and returns an error if any occur during creation.
+//
+//	in example/insert sql
 func Create_ad(ad model.Ad) error {
 	var err error
 	//Insert ad
+	// Begin a transaction to ensure data consistency
 	tx := Db.Begin() // Begin a transaction
 
 	defer func() {
@@ -45,6 +131,7 @@ func Create_ad(ad model.Ad) error {
 			flushAllAdsCache()
 		}
 	}()
+	// Insert the ad record with or without age fields
 	if ad.Conditions[0].AgeEnd == 0 || ad.Conditions[0].AgeStart == 0 {
 		err := Db.Exec("INSERT INTO Ads (title, start_at, end_at) VALUES (?, ?, ?)", ad.Title, ad.StartAt, ad.EndAt).Error
 		if err != nil {
@@ -59,12 +146,14 @@ func Create_ad(ad model.Ad) error {
 		}
 
 	}
+	// Retrieve the newly created ad ID
 	var adID int
 	err = Db.Raw("SELECT @@IDENTITY AS adID;").Scan(&adID).Error
 
 	if err != nil {
 		return err
 	}
+	// Create the ad's conditions using a separate function
 	err = Create_condition(ad.Conditions[0], adID)
 	if err != nil {
 		return err
@@ -73,25 +162,11 @@ func Create_ad(ad model.Ad) error {
 	return nil
 
 }
-func flushAllAdsCache() error {
 
-	// Implement logic to connect to Redis and flush the cache (e.g., using FlushAll)
-	// Consider alternative cache invalidation strategies for better performance
-
-	return redisClient.FlushAll(context.Background()) // Consider setting a suitable expiration time // Replace with actual implementation
-}
-
-func get_clause(data []string) string {
-
-	Clause := ""
-	for _, Code := range data {
-		Clause += "'" + Code + "',"
-	}
-	Clause = Clause[:len(Clause)-1] // Remove trailing comma
-	return Clause
-}
+// CreateAdConditions creates associations between an ad and its target conditions in the database.
+// It takes a Condition struct and the ad ID and returns an error if any occur during creation.
 func Create_condition(condition model.Condition, adId int) error {
-
+	// Concurrently fetch clauses for faster data retrieval (if applicable
 	var (
 		err                                         error
 		countryClause, platformClause, genderClause string
@@ -122,6 +197,8 @@ func Create_condition(condition model.Condition, adId int) error {
 	// genderClause = get_clause(condition.Gender)
 	// countryClause = get_clause(condition.Country)
 	// fmt.Println(genderClause, countryClause, platformClause)
+
+	// Construct the SQL query dynamically
 	sql_query := `INSERT INTO AdConditions (ad_id, condition_id)
 		SELECT ?, c.id
 	FROM Conditions c
@@ -133,8 +210,8 @@ func Create_condition(condition model.Condition, adId int) error {
 	    c.type = 'platform' AND c.value IN (` + platformClause + `)
 	);`
 
-	fmt.Println("Create condition")
-	// fmt.Println(sql_query)
+	// fmt.Println("Create condition")
+	// Execute the query to create ad condition records
 	err = Db.Exec(sql_query, adId).Error
 	if err != nil {
 		return err
@@ -143,45 +220,59 @@ func Create_condition(condition model.Condition, adId int) error {
 	return nil
 }
 
+//
+//find ad
+//
+
+// eample of search sql
+// ..
+//
+// find_adBysql retrieves ads matching given search conditions from the database.
+// It dynamically constructs a SQL query based on the provided criteria,
+// handling various combinations of age, gender, country, and platform conditions.
+//
+// It also caches the retrieved results in Redis for potential future performance gains.
+//
+// Args:
+//
+//	condition (model.Search_Condition): The search criteria to match against ads.
+//
+// Returns:
+//
+//	[]model.Result (nil on error): An array of matching ad results.
+//	error: Any error encountered during database retrieval or caching.
 func find_adBysql(condition model.Search_Condition) ([]model.Result, error) {
-
+	// Initialize variables for dynamic query construction
 	num_param := 0
-	params := []interface{}{} // Initial parameter
+	params := []interface{}{} //Parameters for query execution
 
-	whereClause := ("WHERE a.start_at <= NOW() AND a.end_at >= NOW() ") // Initial WHERE clause
-	typeClause := "AND ("
-
+	whereClause := ("WHERE a.start_at <= NOW() AND a.end_at >= NOW() ") // Base WHERE clause
+	typeClause := "AND ("                                               // Clause for handling multiple conditions
+	// Add age condition if specified
 	if condition.Age != 0 {
 
 		whereClause += "AND (a.min_age <= ? AND a.max_age >= ?) "
-		// str := fmt.Sprintf("%d", condition.Age)
+
 		params = append(params, condition.Age, condition.Age)
 
 	}
 
-	// Add conditions based on Search_Condition
+	// Add gender condition if specified
 	if len(condition.Gender) > 0 {
 		num_param++
 
 		typeClause += "(c.type = 'gender' AND c.value = (?)) "
 		params = append(params, condition.Gender)
-	} //else {
-	// 	num_param++
-	// 	whereClause += "OR (c.type = 'gender' AND c.value IN (?, ?)) "
-	// 	params = append(params, "M", "F")
-	// }
+	}
+	// Add country condition if specified, handling "ALL" value
 	if len(condition.Country) > 0 {
 		num_param++
-		// print("how much", len(condition.Country)*2-1)
-		// countryPlaceholders := strings.Repeat("?,", len(condition.Country))[:len(condition.Country)*2-1]
-		// whereClause += fmt.Sprintf("OR (c.type = 'country' AND c.value IN (%s)) ", countryPlaceholders)
 		typeClause += "OR (c.type = 'country' AND (c.value = (? ) or c.value ='ALL')) "
 		params = append(params, condition.Country)
 
-		// for _, country := range condition.Country {
-		// 	params = append(params, country)
-		// }
 	}
+
+	// Add platform condition if specified
 	if len(condition.Platform) != 0 {
 		num_param++
 
@@ -196,12 +287,14 @@ func find_adBysql(condition model.Search_Condition) ([]model.Result, error) {
 		// 	params = append(params, platform)
 		// }
 	}
-
+	// Construct the final SQL query
 	var sql_query string
 	sql_query = "SELECT a.title,a.end_at" +
 		" FROM Ads a " +
 		" INNER JOIN AdConditions ac ON a.id = ac.ad_id " +
 		" INNER JOIN Conditions c ON ac.condition_id = c.id "
+
+		// Complete conditional clauses and grouping if necessary
 	if num_param != 0 {
 		typeClause += ")"
 		// fmt.Println("type \n", typeClause)
@@ -215,25 +308,18 @@ func find_adBysql(condition model.Search_Condition) ([]model.Result, error) {
 
 		sql_query += whereClause
 	}
-	//order by asc
+	// Add ordering and finalization
 	sql_query += ` ORDER BY a.end_at asc`
-
-	// end of sql_query
 	sql_query += ";"
-	// sql_query = `
 
-	// select * from Ads;
-	// `
-	// fmt.Println(sql_query)
-	// fmt.Println(params...)
-
+	// Execute the query, handle errors, and cache results
 	var ads []model.Result
-
 	err := Db.Raw(sql_query, params...).Scan(&ads).Error
 	if err != nil {
 
 		return nil, err
 	}
+	// Cache results in Redis
 	err = redisSetAd(getSearchString(condition), ads)
 	if err != nil {
 		fmt.Println("cache fail")
@@ -241,41 +327,17 @@ func find_adBysql(condition model.Search_Condition) ([]model.Result, error) {
 	return ads, err
 }
 
-func redisSetAd(key string, ads []model.Result) error {
-	data, err := json.Marshal(ads)
-
-	if err != nil {
-		return err
-	}
-	return redisClient.Set(context.Background(), key, data, 0) // Consider setting a suitable expiration time
-}
-func redisGetAd(key string) ([]model.Result, error) {
-	result, err := redisClient.Get(context.Background(), key)
-	// fmt.Println("get error", err)
-	if result == "" {
-		// fmt.Println("leave nil")
-		return nil, err // Key not found in cache
-
-	} else if err != nil {
-		// fmt.Println("leave != nil")
-
-		return nil, err
-	}
-	// fmt.Println(" success get res")
-	var ads []model.Result
-	if err := json.Unmarshal([]byte(result), &ads); err != nil {
-		return nil, err
-	}
-	// fmt.Println(key, "get ", ads)
-
-	return ads, nil
-}
-
-// /
-func getSearchString(condition model.Search_Condition) string {
-	return fmt.Sprintf("%d_%s_%s_%s", condition.Age, condition.Gender, condition.Country, condition.Platform)
-}
-
+// find_adByredis attempts to retrieve ads matching given search conditions from Redis.
+// It uses a uniquely generated key based on the condition to fetch data from Redis.
+//
+// Args:
+//
+//	condition (model.Search_Condition): The search criteria to match against ads.
+//
+// Returns:
+//
+//	[]model.Result (nil on error): An array of matching ad results if found in Redis.
+//	error: Any error encountered during Redis retrieval.
 func find_adByredis(condition model.Search_Condition) ([]model.Result, error) {
 	key := getSearchString(condition)
 	ads, err := redisGetAd(key)
@@ -287,32 +349,31 @@ func find_adByredis(condition model.Search_Condition) ([]model.Result, error) {
 
 }
 
-// Package  ads
-
-// FunctionName explains the function's purpose and behavior.
+// Find_ad searches for ads matching the given search conditions.
+// It prioritizes retrieving results from Redis for faster response times,
+// falling back to a database query if Redis is unavailable or the data is missing.
 //
 // Args:
-//   * param1 model.Search_Condition - input  condition
-//   * param2 (type) - Description of the second parameter with type information.
+//
+//	condition (model.Search_Condition): The search criteria to match against ads.
 //
 // Returns:
 //
-//   * []model.Result
-//   * error
-//
-//
-
+//	[]model.Result (nil on error): An array of matching ad results.
+//	error: Any error encountered during the search process.
 func Find_ad(condition model.Search_Condition) ([]model.Result, error) {
 
 	var (
 		err error
 		ads []model.Result
 	)
+	// Attempt to fetch ads from Redis for faster retrieval
 	ads, err = find_adByredis(condition)
 	if err != nil {
 
 		return nil, err
 	}
+	// If Redis fails or data is missing, fallback to database query
 	if ads == nil {
 		fmt.Println("redis fail")
 		ads, err = find_adBysql(condition)
@@ -327,6 +388,20 @@ func Find_ad(condition model.Search_Condition) ([]model.Result, error) {
 	return getSlice(ads, condition.Limit, condition.Offset), err
 
 }
+
+// getSlice extracts a slice from a given slice of model.Result objects,
+// applying limit and offset for pagination purposes.
+//
+// Args:
+//
+//	data ([]model.Result): The original slice of data to be sliced.
+//	limit (int): The maximum number of elements to include in the returned slice.
+//	offset (int): The starting index for the slice, indicating where to begin extracting elements.
+//
+// Returns:
+//
+//	[]model.Result: The sliced portion of the data, adhering to the given limit and offset.
+//	nil: If the limit is specified as 0, an empty slice is returned.
 func getSlice(data []model.Result, limit, offset int) []model.Result {
 	if limit == 0 {
 		return nil // Return empty slice for limit 0
